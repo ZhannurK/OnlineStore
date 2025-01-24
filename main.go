73ef5ba724edd4d81083f1d68e8731f2300ce3e6
Zhannur Kainedin <230030@astanait.edu.kz>
2025-01-24 19:27:24 +0500

    added allow cors middleware

--- main.go
+++ main.go
@@ -68,12 +68,19 @@ func init() {
 // --------------------------------------------------------
 // MAIN
 // --------------------------------------------------------
-
+func allowCORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+	})
+}
 func main() {
 
 	jwtKey = []byte(os.Getenv("JWTSECRET"))
 
 	r := mux.NewRouter()
+
+	r.Use(allowCORSMiddleware)
 
 	// Static file routes
 	r.HandleFunc("/shoes", func(w http.ResponseWriter, r *http.Request) {
