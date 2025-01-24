9e8f5c8daca59d4f543474a7522183e65ac1a5cf
Zhannur Kainedin <230030@astanait.edu.kz>
2025-01-24 19:34:55 +0500

    fixed again 2 allow cors middleware

--- main.go
+++ main.go
@@ -70,7 +70,6 @@ func init() {
 // --------------------------------------------------------
 func allowCORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
